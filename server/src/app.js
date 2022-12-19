a1e595ca5f058feb24d34e1d9406515c2f8e169e
Francismiko <1485059943@qq.com>
2022-12-19 12:52:51 +0800

    patch

--- server/src/app.js
+++ server/src/app.js
@@ -17,10 +17,10 @@ app.use(cors({ // 指定一个或多个可以跨域的域名
     allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],  // 设置所允许的HTTP请求方法
     allowHeaders: ['Content-Type', 'Authorization', 'Accept'],  // 设置服务器支持的所有头信息字段
     exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] // 设置获取其他自定义字段
-}));
-app.use(logger());
-app.use(koaBody());
-app.use(routes.routes()).
-    use(routes.allowedMethods());
+})).
+    use(logger()).
+    use(koaBody()).
+    use(routes.routes()).
+    use(routes.allowedMethods())
 
 module.exports = app
