f19cc435ee3028a8bd836aba1f3a512b242bec15
Francismiko <1485059943@qq.com>
2023-03-29 19:55:12 +0800

    add jsDocs

--- server/src/service/business.js
+++ server/src/service/business.js
@@ -1,6 +1,16 @@
 const sequelize_init_ = require('../lib/sequelize');
 
 class BusinessService {
+  /**
+   * 查询数据表结构
+   *
+   * @param {string} host - 地址
+   * @param {string} port - 端口
+   * @param {string} user - 用户名
+   * @param {string} password - 登录密码
+   * @param {string} database - 连接的数据库
+   * @returns {Array} 返回包含数据表结构对象的数组
+   */
   async queryTables(host, port, user, password, database) {
     // 初始化sequelize连接池
     const sequelize = await sequelize_init_(host, port, user, password, database);
